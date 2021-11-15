11a01f701b473a85d8b55645f717aaae22e1d9b1
Eze-ar <93055888+Eze-ar@users.noreply.github.com>
2021-11-15 09:44:53 -0300

    Add files via upload

---
+++ persona.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-11-2021 a las 13:39:50
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `base_de_datos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `apellido` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `edad` tinyint(2) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `provincia` varchar(30) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES
+(5, 'Alfredo', 'Cornejo', 40, '2021-11-15 12:35:52', 'Santa Fe'),
+(6, 'Emilio', 'Castro', 52, '2021-11-15 12:35:52', 'CABA'),
+(7, 'Emilia', 'Atila', 35, '2021-11-15 12:35:52', 'Buenos Aires'),
+(8, 'Virginia', 'Mar', 66, '2021-11-15 12:35:52', 'Tucumán'),
+(9, 'Maximo', 'Gil', 36, '2021-11-15 12:35:52', 'La Pampa');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
